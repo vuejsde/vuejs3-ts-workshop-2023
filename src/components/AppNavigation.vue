3d57d3ed81b99b2d9fc14cf2f97d5d85dee9c208
Antony Konstantinidis <ak@digitalessence.de>
2023-06-30 15:03:45 +0200

    Create your first component

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <ul>
+      <li><strong>Bookmonkey Vue</strong></li>
+    </ul>
+    <ul>
+      <li><a href="#">Books</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#" role="button">Login</a></li>
+    </ul>
+  </nav>
+</template>
