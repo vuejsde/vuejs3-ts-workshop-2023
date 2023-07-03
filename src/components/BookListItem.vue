10d98d052418b720fd59dd6f7c1853b308a8b2b3
Antony Konstantinidis <ak@digitalessence.de>
2023-07-03 10:37:46 +0200

    Use props

---
+++ src/components/BookListItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+type Props = {
+  title: string;
+  isbn: string;
+  numPages?: number;
+  cover?: string;
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <tr>
+    <td>{{ isbn }}</td>
+    <td>{{ title }}</td>
+    <td>{{ numPages }}</td>
+    <td><img :src="cover" width="100" /></td>
+  </tr>
+</template>
