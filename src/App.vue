43d5d774ff59e388f5029ee6c28755e04e80e3e5
Antony Konstantinidis <ak@digitalessence.de>
2023-07-04 10:24:26 +0200

    Add routes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import AppNavigation from './components/AppNavigation.vue'
-import BookList from './components/BookList.vue'
 </script>
 
 <template>
   <AppNavigation />
-  <BookList />
+  <RouterView />
 </template>
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,19 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import BookList from './pages/BookList.vue'
+import AboutPage from './pages/AboutPage.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [],
+  routes: [{
+    path: '/',
+    alias: '/books',
+    component: BookList,
+    name: 'home',
+  }, {
+    path: '/about',
+    component: AboutPage,
+    name: 'about',
+  }],
 })
 
 export default router;
