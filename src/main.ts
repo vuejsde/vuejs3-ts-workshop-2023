7808b7e633394bc08fe416a0868e13012b1f56e2
Antony Konstantinidis <ak@digitalessence.de>
2023-09-25 10:30:47 +0200

    Install and setup pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import router from './router'
 import App from './App.vue'
 
+const store = createPinia();
+
 const app = createApp(App)
 app.use(router)
+app.use(store);
 app.mount('#app')
