b8d5348f302b2f4a5ae674e1303c48b8ac403027
Antony Konstantinidis <ak@digitalessence.de>
2023-09-26 12:06:51 +0200

    Accessing and using multiple stores

--- src/stores/books.ts
+++ src/stores/books.ts
@@ -1,26 +1,28 @@
 import { defineStore } from 'pinia'
 import { watch, ref, computed } from 'vue'
 import type { Book } from '../types'
+import { useAuthStore } from './auth'
 
 export const useBooksStore = defineStore('books', () => {
-  const url = "https://bookmonkey-read-only.onrender.com/books";
+  const auth = useAuthStore()
   const books = ref<Book[]>([])
   const isLoaded = ref(false)
-
   const userInput = ref('')
+
+  const url = computed(() => `https://bookmonkey-read-only.onrender.com/users/${auth.id}/books`)
 
   async function filterBooks(value: string) {
     isLoaded.value = false
     books.value = []
 
-    const response = await fetch(`${url}?q=${value}`)
+    const response = await fetch(`${url.value}?q=${value}`)
     books.value = await response.json() as Book[]
     isLoaded.value = true
   }
 
   async function getBooks() {
     userInput.value = ''
-    const response = await fetch(`${url}?_start=0&_end=50`)
+    const response = await fetch(`${url.value}?_start=0&_end=50`)
     books.value = await response.json() as Book[]
     isLoaded.value = true
   }
