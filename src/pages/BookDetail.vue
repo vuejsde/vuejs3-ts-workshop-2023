d15bd2b87a9571a1184917e3813678ece075435c
Antony Konstantinidis <ak@digitalessence.de>
2024-03-07 15:12:53 +0100

    bonus solution

--- src/pages/BookDetail.vue
+++ src/pages/BookDetail.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
 import { onMounted, ref, watch } from 'vue'
-import { onBeforeRouteLeave } from 'vue-router';
+import { onBeforeRouteLeave, useRouter, type RouteLocationNormalized } from 'vue-router';
 import type { Book } from '../types'
+import ConfirmationDialog from '@/components/ConfirmationDialog.vue';
 
 const props = defineProps<{
   isbn: string
@@ -27,9 +28,34 @@ async function fetchBook(isbn: string) {
 onMounted(() => fetchBook(props.isbn as string))
 watch(() => props.isbn, (newIsbn) => fetchBook(newIsbn as string))
 
-onBeforeRouteLeave(() => {
-  return window.confirm('Do you really want to do that?');
+onBeforeRouteLeave((to) => {
+  if (hasDialogConfirmed.value) {
+    return true;
+  } else {
+    dialogIsVisible.value = true;
+    intendedRoute.value = to;
+    return false;
+  }
 })
+
+const router = useRouter();
+const dialogIsVisible = ref(false)
+const hasDialogConfirmed = ref(false)
+const intendedRoute = ref<RouteLocationNormalized | null>(null)
+
+function reject() {
+  dialogIsVisible.value = false;
+  hasDialogConfirmed.value = false;
+}
+
+function confirm() {
+  dialogIsVisible.value = false;
+  hasDialogConfirmed.value = true;
+
+  if (intendedRoute.value) {
+    router.push(intendedRoute.value.fullPath);
+  }
+}
 </script>
 
 <template>
@@ -43,4 +69,14 @@ onBeforeRouteLeave(() => {
       We couldn't find any book with the ISBN <strong>{{ isbn }}</strong>
     </span>
   </template>
+  <ConfirmationDialog :show="dialogIsVisible" @cancel="reject" @confirm="confirm">
+
+    <template #header>
+      ⛔️ Unsaved changes
+    </template>
+
+    <template #body>
+      Do you really want to leave the page?
+    </template>
+  </ConfirmationDialog>
 </template>
